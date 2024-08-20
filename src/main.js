0d7b49b13ce4288b3a90a5d08d6a1e4a8cdf4a35
Mel <57514727+Mel1207@users.noreply.github.com>
2024-08-20 18:50:38 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:4000/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
+
+app.use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Users</h1>
+    <div v-for="item in users" :key="item.id">
+      <p>{{ item.name }}</p>
+      <span>{{ item.id }}</span>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+<script setup>
+import gql from 'graphql-tag'
+import { watchEffect, computed } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+const allUsersQuery = gql`
+  query {
+    users {
+      id
+      name
+    }
   }
-}
+`
+
+const { result } = useQuery(allUsersQuery)
+// const users = useResult(result, [], result.users)
+
+const users = computed(() => result.value?.users ?? [])
+
 </script>
